a09c2669d18812e15b609a02df782f12a2dc71a6
Miguel Frias <isc.miguelfrias@gmail.com>
2014-01-16 17:52:27 -0700

    Remove old permutation function

--- js/modules/charParser.js
+++ js/modules/charParser.js
@@ -45,40 +45,6 @@
             return result;
         };
 
-        // Original: https://gist.github.com/md2perpe/8210411
-        // function _getCombinations (list) { // AKA Permutation
-
-        //     // Empty list has one permutation
-        //     if (list.length == 0) {
-        //         return [[]];
-        //     }
-
-        //     var result = [];
-
-        //     for (var i=0; i<list.length; i++) {
-
-        //         // Clone list (kind of)
-        //         var copy = Object.create(list);
-
-        //         // Cut one element from list
-        //         var head = copy.splice(i, 1);
-
-        //         // Permute rest of list
-        //         var rest = _getCombinations(copy);
-
-        //         // Add head to each permutation of rest of list
-        //         for (var j=0; j<rest.length; j++) {
-
-        //             var next = head.concat(rest[j]);
-        //             result.push(next);
-
-        //         }
-
-        //     }
-
-        //     return result;
-        // };
-
         function _mapLetter (value, key, list) {
 
             if (value === '0' || value === '1' ) {
